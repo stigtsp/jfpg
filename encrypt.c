1379e780b154283301be2fc0e172e1ae94df95c6
Joe Fierro <jsf122@scarletmail.rutgers.edu>
2016-06-18 09:16:25 -0400

    sizeof() should be strlen() for explicit_bzero()

--- encrypt.c
+++ encrypt.c
@@ -127,7 +127,7 @@ symcrypt(unsigned char *ctext_buf, unsigned char *pad_ptext_buf, struct hdr *hdr
 	if (crypto_scrypt((unsigned char *)pass, strlen(pass), hdr->nonce, sizeof(hdr->nonce),
 	    hdr->rounds, hdr->r, hdr->p, symkey, sizeof(symkey)) != 0)
 		err(1, "error hashing key");
-	explicit_bzero(pass, sizeof(pass));
+	explicit_bzero(pass, strlen(pass));
 
 	if (crypto_secretbox(ctext_buf, pad_ptext_buf, hdr->padded_len,
             hdr->nonce, symkey) != 0)
