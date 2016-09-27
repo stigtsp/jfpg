8ff48ba04425f738eaaa12b7337cdb93fc1791ba
an0dos <fierrojs@verizon.net>
2016-09-26 22:03:40 -0400

    change default extension to .jfpg to more closely follow what GPG does. I'm leaving signed files with the .signed extension for now.

--- defines.h
+++ defines.h
@@ -1,5 +1,5 @@
 #define FILENAME_SIZE   255
-#define EXT     ".xsalsa20"
+#define EXT     ".jfpg"
 #define SIGNEXT ".signed"
 #define PUB     "-pubkey.curve25519"
 #define SEC     "-secretkey.curve25519"
